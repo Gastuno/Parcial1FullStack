2ab2b1aaa43744363d34ea1e03e948c2eecd08c5
UNO7987 <109447917+UNO7987@users.noreply.github.com>
2026-08-19 23:10:24 -0300

    CSS

--- fsp1front/src/Detalle.jsx
+++ fsp1front/src/Detalle.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import ReviewsBox from './ReviewList.jsx'
+import './Detalle.css'
 
 function Detalle() {
   const { id } = useParams()
@@ -14,22 +15,28 @@ function Detalle() {
   }, [id])
 
   if (!movie) {
-    return <main>Cargando...</main>
+    return <main className="details">Cargando...</main>
   }
 
   return (
-    <main className="main">
-      <Link to="/">Volver</Link>
-      <td>
+    <main className="details">
+      <Link className="goBack" to="/">Volver</Link>
+      <section className="movieDetail">
         {movie.poster_path ? (
           <img
-            className="moviePoster"
+            className="movieDetailPoster"
             src={`${posterUrl}${movie.poster_path}`}
-          />) : ('N/A')}
-      </td>
-      <h1>{movie.title}</h1>
-      <p>Year: {movie.release_date?.slice(0, 4) || 'N/A'}</p>
-      <p>Rating: {movie.vote_average?.toFixed(1) || 'N/A'}</p>
+            alt={`Portada de ${movie.title}`}
+          />
+        ) : (
+          <div className="movieDetailPoster2">Sin portada</div>
+        )}
+        <div className="movieDetailInfo">
+          <h1>{movie.title}</h1>
+          <p><span className="movieDetailInfo2">Año:</span> {movie.release_date?.slice(0, 4) || 'N/A'}</p>
+          <p><span className="movieDetailInfo2">Rating:</span> {movie.vote_average?.toFixed(1) || 'N/A'}</p>
+        </div>
+      </section>
       <ReviewsBox movieId={id} />
     </main>
   )
